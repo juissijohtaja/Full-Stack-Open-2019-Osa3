b8b818a8bece2b0a509d7a68cd140c0782c78516
JuissiJohtaja <juha.koivu@outlook.com>
2019-06-13 14:06:42 +0300

    Done 3.8

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
-app.use(bodyParser.json())
-app.use(morgan('tiny'))
+morgan.token('body', (req) => JSON.stringify(req.body));
+
+app.use(bodyParser.json());
+app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'))
+
 
 let persons = [
       {
