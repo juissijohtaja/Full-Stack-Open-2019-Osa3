9eb24e7867c5f8882c4abbb2e05a03b8a2fa0661
JuissiJohtaja <juha.koivu@outlook.com>
2019-06-19 17:21:21 +0300

    Done

---
+++ build/precache-manifest.e6338d3ea1cf71380f7688588d75b0b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773110aafcefe822e22587e44a6e2c7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a4bac759ea71ea16412",
+    "url": "/static/css/main.7db73bd2.chunk.css"
+  },
+  {
+    "revision": "368366dbb886b4cdae6c",
+    "url": "/static/js/2.50d53a4f.chunk.js"
+  },
+  {
+    "revision": "3a4bac759ea71ea16412",
+    "url": "/static/js/main.5265e547.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(morgan(':method :url :body :status :res[content-length] - :response-time
 
 const cors = require('cors')
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
       {
